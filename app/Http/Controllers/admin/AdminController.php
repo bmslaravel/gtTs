22cc775ef8254401c8caa5cc6bb7a41539622dd1
bms <18200172438@163.com>
2016-12-13 00:31:27 +0800

    admin

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bms
+ * Date: 2016/12/13
+ * Time: 0:29
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index(){
+        
+    }
+}
